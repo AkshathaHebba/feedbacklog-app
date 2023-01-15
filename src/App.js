3c8cb5094eeb19f975e14e86f21dc4fd5a8c4739
Akshatha Hebbar <akshathahebbar1234@gmail.com>
2023-01-14 18:45:34 -0800

    Resolved error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { useState } from 'react'
-import {BrowserRouter as Router, Route, Routes,NavLink} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 //Browser Router - Html file history API to keep our UI with url
 import Header from './components/Header'
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from "./components/FeedbackList"
 import FeedbackForm from "./components/FeedbackForm"
-import { v4 as uuidv4} from 'uuid'
 import AboutPage from "./pages/AboutPage";
 import {FeedbackProvider} from "./context/FeedbackContext";
 import AboutIconLink from './components/AboutIconLink'
-import Card from './components/shared/Card'
 import Post from "./components/Post";
 
 function App() {
-  const [feedback,setFeedback] = useState(FeedbackData) //App level state
+  //const [feedback,setFeedback] = useState(FeedbackData) //App level state
   return (
       <FeedbackProvider>
           <Router>
